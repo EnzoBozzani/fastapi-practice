7c7d8848c4d6e7a84b28f20ee41131b61165e594
Enzo Bozzani <bozzanienzo@gmail.com>
2024-08-12 14:34:08 -0300

    feat: create add admin route

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.responses import JSONResponse
 from typing import Annotated
 from dotenv import load_dotenv
 from fastapi.security import OAuth2PasswordRequestForm
 from datetime import timedelta
 
 from routers import windows, speakers, lectures
-from database.schemas import Token
-from database.queries.admin import authenticate_admin
+from database.schemas import Token, AdminCreate
+from database.queries.admin import authenticate_admin, add_admin
 from utils.constants import ACCESS_TOKEN_EXPIRE_MINUTES
 from utils.main import create_access_token
 
@@ -21,7 +22,7 @@ app.include_router(speakers.router)
 app.include_router(lectures.router)
 
 
-@app.post('/login', response_model=Token, tags=["Auth"])
+@app.post('/login', response_model=Token, tags=["Admin"])
 async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     user = authenticate_admin(form_data.username, form_data.password)
 
@@ -39,3 +40,22 @@ async def login(form_data: Annotated[OAuth2PasswordRequestForm, Depends()]):
     )
 
     return Token(access_token=access_token, token_type="Bearer")
+
+
+@app.post('/add-admin', tags=["Admin"])
+def create_admin(adminCreate: AdminCreate):
+    new_admin = add_admin(
+        email=adminCreate.email,
+        password=adminCreate.password
+    )
+
+    if new_admin is None:
+        return JSONResponse(
+            status_code=400,
+            content={'error': f'Admin ${adminCreate.email} already exists!'}
+        )
+
+    return JSONResponse(
+        status_code=200,
+        content={'success': 'Created new admin!'}
+    )
--- database/queries/admin.py
+++ database/queries/admin.py
@@ -1,6 +1,6 @@
 from sqlmodel import Session, select
 
-from utils.main import verify_password
+from utils.main import verify_password, get_password_hash
 from database.models import Admin
 from database.main import engine
 
@@ -24,4 +24,24 @@ def get_admin(email: str):
         statement = select(Admin).where(Admin.email == email)
         admin = db.exec(statement).first()
 
+        return admin
+
+
+def add_admin(email: str, password: str):
+    existing_admin = get_admin(email)
+
+    if existing_admin is not None:
+        return None
+
+    hashed_password = get_password_hash(password)
+
+    admin = Admin(
+        email=email,
+        hashed_password=hashed_password
+    )
+
+    with Session(engine) as db:
+        db.add(admin)
+        db.commit()
+        db.refresh(admin)
         return admin
